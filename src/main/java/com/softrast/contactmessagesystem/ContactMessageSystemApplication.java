67a1afbf2b659c58d7c223a8569dc678c8326816
Bogdan Pryadko <Bogdanpryadko1@gmail.com>
2025-06-16 12:30:40 +0300

    first commit

---
+++ src/main/java/com/softrast/contactmessagesystem/ContactMessageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.softrast.contactmessagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMessageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactMessageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softrast/contactmessagesystem/ContactMessageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softrast.contactmessagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactMessageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
