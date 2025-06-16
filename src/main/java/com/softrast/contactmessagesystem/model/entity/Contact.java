3ee9304deb5b0aa3268e54ca1689702ab0c62337
Bogdan Pryadko <Bogdanpryadko1@gmail.com>
2025-06-16 12:35:34 +0300

    feat(model) add Contact entity

---
+++ src/main/java/com/softrast/contactmessagesystem/model/entity/Contact.java
@@ -0,0 +1,25 @@
+package com.softrast.contactmessagesystem.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String fullName;
+
+}
